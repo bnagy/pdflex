50dc8f0f2ccd227ec241f589069e9d3da0a26289
ben <git@ben.iagu.net>
2015-04-12 10:22:06 +1200

    kick into beta

--- cmd/pdfshrink/main.go
+++ cmd/pdfshrink/main.go
@@ -25,6 +25,11 @@ var trailer = []byte("trailer")
 var pref85 = []byte("<~")
 var suff85 = []byte("~>")
 
+var (
+	flagStrict *bool = flag.Bool("strict", false, "Abort on xref parsing errors etc")
+	flagMax    *int  = flag.Int("max", MAXDATA, "Trim streams whose size is greater than this value")
+)
+
 func fixStartXref(in []byte) []byte {
 
 	sxrIdx := bytes.LastIndex(in, startxref)
@@ -39,41 +44,68 @@ func fixStartXref(in []byte) []byte {
 
 	scratch := []byte{}
 	scratch = append(scratch, in[:sxrIdx]...)
-	scratch = append(scratch, []byte(fmt.Sprintf("startxref\n%d\n%%%%EOF\n", xrIdx))...)
+	scratch = append(scratch, []byte(fmt.Sprintf("startxref\n%d\n%%%%EOF", xrIdx))...)
 	return scratch
 }
 
 func fixXrefs(in []byte) []byte {
+
 	out := new(bytes.Buffer)
 	tr := bytes.LastIndex(in, trailer)
 	xr := bytes.LastIndex(in[:tr], xref)
+	if xr < 0 || tr < 0 || tr < xr {
+		return in
+	}
 	xrSection := in[xr:tr]
+
+	// Try to normalize the xrefs so lines are delimited by one \n. This works
+	// for \n, \r, \r\n...
 	normalized := strings.Replace(string(xrSection), "\r", "\n", -1)
 	normalized = strings.Replace(normalized, "\n\n", "\n", -1)
+
+	// Validate / parse the header rows
 	scanner := bufio.NewScanner(strings.NewReader(normalized))
 	scanner.Scan()
 	ff := strings.Fields(scanner.Text())
 	if ff[0] != "xref" {
-		log.Fatalf("Corrupt xref section\n%#v\n", string(xrSection))
+		if *flagStrict {
+			log.Fatalf("[STRICT] Corrupt xref section\n%#v\n", string(xrSection))
+		}
+		return in
 	}
 	fmt.Fprintln(out, scanner.Text())
 	scanner.Scan()
 	ff = strings.Fields(scanner.Text())
 	if len(ff) < 2 {
-		log.Fatalf("Corrupt xref section\n%#v\n", string(xrSection))
+		if *flagStrict {
+			log.Fatalf("[STRICT] Corrupt xref section\n%#v\n", string(xrSection))
+		}
+		return in
 	}
 	expected, err := strconv.Atoi(ff[1])
 	if err != nil {
-		log.Fatalf("Corrupt xref section\n%#v\n", string(xrSection))
+		if *flagStrict {
+			log.Fatalf("[STRICT] Corrupt xref section\n%#v\n", string(xrSection))
+		}
+		return in
 	}
 
+	// Parse the xrefs entries and try to fix up indirect ref offsets
 	for i := 0; i < expected; i++ {
 		if !scanner.Scan() {
-			log.Fatalf("Short xref section\n")
+			if *flagStrict {
+				log.Fatalf("[STRICT] Short xref section\n")
+			}
+			return in
 		}
 		ff := strings.Fields(scanner.Text())
+		// Get indirect refs like 0000037118 00000 n
+		// Don't know what 0000000389 00001 f refs are
 		if ff[2] == "n" {
 			idx := -1
+			// Do it this way instead of using a regex, because the multi-line
+			// regexes / anchors seem wonky for PDFs that use \r as a "bare"
+			// line delimeter
 			idx = bytes.Index(in, []byte(fmt.Sprintf("\n%d 0 obj", i)))
 			if idx < 0 {
 				idx = bytes.Index(in, []byte(fmt.Sprintf("\r%d 0 obj", i)))
@@ -83,29 +115,35 @@ func fixXrefs(in []byte) []byte {
 				continue
 			}
 		}
-		// in all other cases
+		// Not an indirect ref OR couldn't find that obj declaration
+		// Emit this line unmodified
 		fmt.Fprintln(out, scanner.Text())
 	}
-	head := in[:xr]
-	tail := in[tr:]
-	final := append(head, out.Bytes()...)
-	final = append(final, tail...)
+
+	// Replace the xrefs with our modified version. The length might be
+	// slightly different if we squeezed one or more \r\n into \n.
+	final := append(in[:xr], out.Bytes()...)
+	final = append(final, in[tr:]...)
 	return fixStartXref(final)
 }
 
 func inflate(s string) (string, error) {
+
 	in := strings.NewReader(s)
 	decom, err := zlib.NewReader(in)
 	if err != nil {
 		return "", err
 	}
+
 	var b bytes.Buffer
-	io.Copy(&b, decom)
+	_, err = io.Copy(&b, decom)
 	decom.Close()
+
 	return b.String(), err
 }
 
 func deflate(s string) (string, error) {
+
 	var b bytes.Buffer
 	w := zlib.NewWriter(&b)
 	_, err := w.Write([]byte(s))
@@ -113,14 +151,17 @@ func deflate(s string) (string, error) {
 	if err != nil {
 		return "", err
 	}
+
 	return b.String(), nil
 }
 
 func un85(s string) (string, error) {
+
 	in := []byte(s)
+	// Caller is expected to trim <~ ~> if present
 	in = bytes.TrimPrefix(in, pref85)
 	in = bytes.TrimSuffix(in, suff85)
-	out := make([]byte, len(in))
+	out := make([]byte, 0, len(in))
 
 	n, _, err := ascii85.Decode(out, in, true)
 	if err != nil {
@@ -151,6 +192,12 @@ func main() {
 		)
 	}
 
+	flag.Parse()
+	if *flagMax < 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
 	for _, arg := range os.Args[1:] {
 
 		raw, err := ioutil.ReadFile(arg)
@@ -167,27 +214,29 @@ func main() {
 				s := i.Val
 
 				if asc85 {
-
 					s, err = un85(s)
 					if err != nil {
-						log.Fatalf("Failed to un85: %s", err)
+						log.Fatalf("[STRICT] Failed to un85: %s", err)
 					}
 				}
 
 				if zipped {
-
 					s2, err := inflate(s)
-
-					if err != nil {
-						log.Fatalf("%d Failed to inflate internal stream: %s", i.Pos, err)
+					if err != nil && *flagStrict {
+						log.Fatalf("[STRICT] Error unzipping internal stream: %s", err)
 					}
+					// If not struct, we ignore any errors here. If it's
+					// unexpected EOF we'll get partial unzipped data, so use
+					// that for truncation. Other errors will read a zero
+					// length string, in which case we fall back to truncating
+					// the original (corrupt) zipped stream.
 					if len(s2) > 0 {
 						s = s2
 					}
 				}
 
-				if len(s) > MAXDATA {
-					s = s[:MAXDATA]
+				if len(s) > *flagMax {
+					s = s[:*flagMax]
 				} else {
 					// write the original string
 					out.WriteString(i.Val)
@@ -199,21 +248,24 @@ func main() {
 				if zipped {
 					s, err = deflate(s)
 					if err != nil {
+						// should never happen, strict mode or not
 						log.Fatalf("Error zipping truncated string: %s", err)
 					}
 				}
 				if asc85 {
 					s, err = re85(s)
 					if err != nil {
+						// ditto
 						log.Fatalf("Error Ascii85ing zipped string: %s", err)
 					}
 				}
 				out.WriteString(s)
 				zipped = false
 				asc85 = false
-			} else {
-				if i.Typ == pdflex.ItemName && i.Val == "/FlateDecode" {
 
+			} else {
+
+				if i.Typ == pdflex.ItemName && i.Val == "/FlateDecode" {
 					zipped = true
 				}
 				if i.Typ == pdflex.ItemName && i.Val == "/ASCII85Decode" {
@@ -225,11 +277,16 @@ func main() {
 			if i.Typ == pdflex.ItemError {
 				break
 			}
-
 		}
 
 		fixed := fixXrefs(out.Bytes())
-		fmt.Println(string(fixed))
+		newfn := strings.TrimSuffix(path.Base(arg), path.Ext(arg)) + "-small" + path.Ext(arg)
+		fmt.Println(newfn)
+		newfn = path.Join(path.Dir(arg), newfn)
+		err = ioutil.WriteFile(newfn, fixed, 0600)
+		if err != nil {
+			log.Fatalf("Unable to write %s: %s", newfn, err)
+		}
 	}
 
 }
